add2e8330cb3cdcd2a5541d1a8158d338a698214
liangzohar <saviourxx@qq.com>
2023-07-31 22:16:46 +0800

    [bsp/chkgv2] add ulog_easyflash support

--- bsp/stm32/stm32f103-atn-chkgv2/applications/main.c
+++ bsp/stm32/stm32f103-atn-chkgv2/applications/main.c
@@ -12,6 +12,11 @@
 #include <rtdevice.h>
 #include <board.h>
 #include "fal.h"
+#include "easyflash.h"
+
+#define DBG_TAG "main"
+#define DBG_LVL DBG_LOG
+#include <rtdbg.h>
 
 /* defined the LED0 pin: PF7 */
 #define LED0_PIN    GET_PIN(C, 13)
@@ -20,6 +25,14 @@ int main(void)
 {
     fal_init();
     easyflash_init();
+    LOG_D("Hello world!");
+    uint8_t data[3] ={1,2,3};
+    LOG_E("error");
+    LOG_W("waring");
+    LOG_I("info");
+    LOG_D("debug");
+    LOG_RAW("unformatted\n");
+    ulog_hexdump("main", 8, data, sizeof(data)); //m
     /* set LED0 pin mode to output */
     rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
 
